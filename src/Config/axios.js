f309d5f23ff2f9d41abdf993749044822729be49
Kevin Alberca <desarrollador.ext3@pymhse.com>
2021-03-14 16:29:37 -0500

    https

--- src/Config/axios.js
+++ src/Config/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 
 export default axios.create({
-    baseURL: "https://aicoronel-backend/api",//process.env.REACT_APP_API,
+    baseURL: "https://aicoronel-backend.gogazo.com/api",//process.env.REACT_APP_API,
    // baseURL: "http://localhost:8000/api",
     headers: {
         'Content-Type': 'application/json',
